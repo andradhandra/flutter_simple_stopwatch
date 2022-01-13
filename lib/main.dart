c8447ecb712aa50f88ee4a7a470bc3b810b3bcfa
andradhandra <andradhandra@gmail.com>
2022-01-13 15:45:31 +0700

    Done basic timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:stopwatch_flutter/ui/stopwatch.dart';
 
 void main() {
@@ -9,7 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+      ),
       home: MyHomePage(),
     );
   }
@@ -20,9 +23,12 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Stopwatch(),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.light,
+      child: Scaffold(
+        body: Center(
+          child: Stopwatch(),
+        ),
       ),
     );
   }
---
+++ lib/ui/stopwatch_renderer.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StopwatchRenderer extends StatefulWidget {
+  const StopwatchRenderer({ Key? key }) : super(key: key);
+
+  @override
+  _StopwatchRendererState createState() => _StopwatchRendererState();
+}
+
+class _StopwatchRendererState extends State<StopwatchRenderer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
