a891c6007169c8e98ec3215e3977d6f3061e072d
andradhandra <andradhandra@gmail.com>
2022-01-17 16:05:07 +0700

    done solution

--- lib/ui/stopwatch.dart
+++ lib/ui/stopwatch.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:stopwatch_flutter/ui/reset_button.dart';
+import 'package:stopwatch_flutter/ui/start_stop_button.dart';
 import 'package:stopwatch_flutter/ui/stopwatch_renderer.dart';
 
 class Stopwatch extends StatefulWidget {
@@ -9,18 +11,43 @@ class Stopwatch extends StatefulWidget {
 
 class _StopwatchState extends State<Stopwatch>
     with SingleTickerProviderStateMixin {
-  Duration _elapsedTime = Duration.zero;
+  Duration _currentElapsedTime = Duration.zero;
+  Duration _previousElapsedTime = Duration.zero;
   late final Ticker _ticker;
+  bool _isRunning = false;
+
+  Duration get _elapsed => _previousElapsedTime + _currentElapsedTime;
+
+  void _startStopTime() {
+    setState(() {
+      _isRunning = !_isRunning;
+      if (_isRunning) {
+        _ticker.start();
+      } else {
+        _ticker.stop();
+        _previousElapsedTime += _currentElapsedTime;
+        _currentElapsedTime = Duration.zero;
+      }
+    });
+  }
+
+  void _resetTime() {
+    _ticker.stop();
+    setState(() {
+      _isRunning = false;
+      _currentElapsedTime = Duration.zero;
+      _previousElapsedTime = Duration.zero;
+    });
+  }
 
   @override
   void initState() {
     super.initState();
     _ticker = this.createTicker((elapsed) {
       setState(() {
-        _elapsedTime = elapsed;
+        _currentElapsedTime = elapsed;
       });
     });
-    _ticker.start();
   }
 
   @override
@@ -33,9 +60,34 @@ class _StopwatchState extends State<Stopwatch>
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
       final radius = constraints.maxWidth / 2;
-      return StopwatchRenderer(
-        elapsedTime: _elapsedTime,
-        radius: radius,
+      return Stack(
+        children: [
+          StopwatchRenderer(
+            elapsedTime: _elapsed,
+            radius: radius,
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SizedBox(
+              height: 80,
+              width: 80,
+              child: StartStopButton(
+                isRunning: _isRunning,
+                onPressed: _startStopTime,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: SizedBox(
+              width: 80,
+              height: 80,
+              child: ResetButton(
+                onPressed: _resetTime,
+              ),
+            ),
+          )
+        ],
       );
     });
   }
